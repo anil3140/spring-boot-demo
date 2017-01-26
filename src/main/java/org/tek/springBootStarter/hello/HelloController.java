c76fa000a96243cc305e591a49c63a3f3c8f4f2e
anil3140 <anil3140@gmail.com>
2017-01-25 19:11:07 -0600

    first commit

---
+++ src/main/java/org/tek/springBootStarter/hello/HelloController.java
@@ -0,0 +1,16 @@
+package org.tek.springBootStarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHi(@RequestParam(value = "name") String name){
+		
+		return "Hi" + name;
+	}
+
+}
